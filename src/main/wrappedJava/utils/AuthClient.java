b929170839a4f0eb06a5c02306ba61b144f6803a
coralewh <corale@whispir.com>
2022-10-20 13:54:19 +0800

    wrapper

---
+++ src/main/wrappedJava/utils/AuthClient.java
@@ -0,0 +1,28 @@
+
+import io.github.cdimascio.dotenv.Dotenv;
+import whispir_sdk_java.ApiClient;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author carlangeloorale
+ */
+public class AuthClient {
+    
+    public static ApiClient createClient() {
+        ApiClient client;
+        client = new ApiClient();
+        Dotenv dotenv = Dotenv.load();
+
+        String API_URL = dotenv.get("API_URL");
+
+        client.setBasePath(API_URL);
+        client.setUsername(dotenv.get("WHISPIR_USERNAME"));
+        client.setPassword(dotenv.get("WHISPIR_PASSWORD"));
+        return client;
+    }
+}
