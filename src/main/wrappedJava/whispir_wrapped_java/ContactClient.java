cea4c3da46ad1b9b822c0992a201e5aa1c7501b6
coralewh <corale@whispir.com>
2022-10-23 19:49:21 +0800

    fix: fix wrapper

--- src/main/wrappedJava/whispir_wrapped_java/ContactClient.java
+++ src/main/wrappedJava/whispir_wrapped_java/ContactClient.java
@@ -18,17 +18,14 @@ import whispir_sdk_java.ApiException;
 public class ContactClient {
 
   private static String USERNAME, PASSWORD, API_URL, WORKSPACE_ID;
-  private static final String CONTENT_TYPE =
-    "application/vnd.whispir.contact-v1+json";
-  private static final String ACCEPT =
-    "application/vnd.whispir.contact-v1+json";
+  private static final String CONTENT_TYPE = "application/vnd.whispir.contact-v1+json";
+  private static final String ACCEPT = "application/vnd.whispir.contact-v1+json";
 
   public ContactClient(
-    String username,
-    String password,
-    String apiUrl,
-    String workspaceId
-  ) {
+      String username,
+      String password,
+      String apiUrl,
+      String workspaceId) {
     ContactClient.USERNAME = username;
     ContactClient.PASSWORD = password;
     ContactClient.API_URL = apiUrl;
@@ -49,92 +46,84 @@ public class ContactClient {
     ContactsApi contact = new ContactsApi(createClient());
     try {
       return contact.postContacts(
-        WORKSPACE_ID,
-        API_KEY,
-        ACCEPT,
-        CONTENT_TYPE,
-        CONTACT
-      );
+          WORKSPACE_ID,
+          API_KEY,
+          ACCEPT,
+          CONTENT_TYPE,
+          CONTACT);
     } catch (ApiException e) {
       throw new Error(e);
     }
   }
 
   public static GetContacts200Response listContacts(
-    String API_KEY,
-    BigDecimal LIMIT,
-    BigDecimal OFFSET,
-    String SORT_ORDER,
-    String SORT_FIELDS,
-    String FIELD_NAME,
-    Boolean CUSTOM_FIELDS
-  ) {
+      String API_KEY,
+      BigDecimal LIMIT,
+      BigDecimal OFFSET,
+      String SORT_ORDER,
+      String SORT_FIELDS,
+      String FIELD_NAME,
+      Boolean CUSTOM_FIELDS) {
     ContactsApi contact = new ContactsApi(createClient());
     try {
       return contact.getContacts(
-        WORKSPACE_ID,
-        API_KEY,
-        ACCEPT,
-        LIMIT,
-        OFFSET,
-        SORT_ORDER,
-        SORT_FIELDS,
-        FIELD_NAME,
-        CUSTOM_FIELDS
-      );
+          WORKSPACE_ID,
+          API_KEY,
+          ACCEPT,
+          LIMIT,
+          OFFSET,
+          SORT_ORDER,
+          SORT_FIELDS,
+          FIELD_NAME,
+          CUSTOM_FIELDS);
     } catch (ApiException e) {
       throw new Error(e);
     }
   }
 
   public static void retrieveContact(
-    String API_KEY,
-    String CONTACT_ID,
-    String FIELDS,
-    String SORT_ORDER,
-    String SORT_FIELDS
-  ) {
+      String API_KEY,
+      String CONTACT_ID,
+      String FIELDS,
+      String SORT_ORDER,
+      String SORT_FIELDS) {
     ContactsApi contact = new ContactsApi(createClient());
     try {
       contact.getContactsById(
-        WORKSPACE_ID,
-        API_KEY,
-        CONTACT_ID,
-        ACCEPT,
-        FIELDS,
-        SORT_ORDER,
-        SORT_FIELDS
-      );
+          WORKSPACE_ID,
+          API_KEY,
+          CONTACT_ID,
+          ACCEPT,
+          FIELDS,
+          SORT_ORDER,
+          SORT_FIELDS);
     } catch (ApiException e) {
       throw new Error(e);
     }
   }
 
   public static void updateContact(
-    String API_KEY,
-    String CONTACT_ID,
-    Contact CONTACT
-  ) {
+      String API_KEY,
+      String CONTACT_ID,
+      Contact CONTACT) {
     ContactsApi contact = new ContactsApi(createClient());
     try {
       contact.putContactsById(
-        WORKSPACE_ID,
-        API_KEY,
-        CONTACT_ID,
-        ACCEPT,
-        CONTENT_TYPE,
-        CONTACT
-      );
+          WORKSPACE_ID,
+          API_KEY,
+          CONTACT_ID,
+          ACCEPT,
+          CONTENT_TYPE,
+          CONTACT);
     } catch (ApiException e) {
       throw new Error(e);
     }
   }
 
   public static void deleteContact(
-    String API_KEY,
-    String CONTACT_ID,
-    String ACTION
-  ) {
+      String API_KEY,
+      String CONTACT_ID,
+      String ACTION) {
     ContactsApi contact = new ContactsApi(createClient());
     try {
       contact.deleteContactsById(WORKSPACE_ID, API_KEY, CONTACT_ID, ACTION);
--- src/main/wrappedJava/whispir_wrapped_java/MessageClient.java
+++ src/main/wrappedJava/whispir_wrapped_java/MessageClient.java
@@ -4,6 +4,10 @@
  */
 package whispir_wrapped_java;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import whispir_sdk_java.ApiClient;
+import org.whispir.api.MessagesApi;
 import org.openapitools.client.model.Message;
 import org.whispir.api.MessagesApi;
 import whispir_sdk_java.ApiClient;
@@ -17,17 +21,14 @@ public class MessageClient {
 
   static String USERNAME;
   private static String PASSWORD, API_URL, WORKSPACE_ID;
-  private static final String CONTENT_TYPE =
-    "application/vnd.whispir.message-v1+json";
-  private static final String ACCEPT =
-    "application/vnd.whispir.message-v1+json";
+  private static final String CONTENT_TYPE = "application/vnd.whispir.message-v1+json";
+  private static final String ACCEPT = "application/vnd.whispir.message-v1+json";
 
   public MessageClient(
-    String username,
-    String password,
-    String apiUrl,
-    String workspaceId
-  ) {
+      String username,
+      String password,
+      String apiUrl,
+      String workspaceId) {
     MessageClient.USERNAME = username;
     MessageClient.PASSWORD = password;
     MessageClient.API_URL = apiUrl;
@@ -46,16 +47,21 @@ public class MessageClient {
 
   public static Message postMessage(Message MESSAGE, String API_KEY) {
     MessagesApi message = new MessagesApi(createClient());
+    Calendar calendar = Calendar.getInstance();
+    SimpleDateFormat dateFor = new SimpleDateFormat("dd/MM/YYYY HH:mm");
+    calendar.add(Calendar.DAY_OF_WEEK, 1);
+
+    System.out.println(dateFor.format(calendar.getTime()));
+
+    MESSAGE.messageType(Message.MessageTypeEnum.SCHEDULED);
+    MESSAGE.scheduleType(Message.ScheduleTypeEnum.ONCE);
+    MESSAGE.scheduleDate(dateFor.format(calendar.getTime()));
+
     try {
-      return message.postMessages(
-        WORKSPACE_ID,
-        API_KEY,
-        CONTENT_TYPE,
-        ACCEPT,
-        MESSAGE
-      );
+      return message.postMessages(WORKSPACE_ID, API_KEY, CONTENT_TYPE, ACCEPT, MESSAGE);
     } catch (ApiException e) {
       throw new Error(e);
     }
   }
+
 }
