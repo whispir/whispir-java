e52b4dea9cd5e69f341d92ec22c8836da64b3ef0
coralewh <corale@whispir.com>
2022-10-24 16:08:55 +0800

    fix: add validation for post message

--- src/main/wrappedJava/whispir_wrapped_java/MessageClient.java
+++ src/main/wrappedJava/whispir_wrapped_java/MessageClient.java
@@ -6,13 +6,10 @@ package whispir_wrapped_java;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import whispir_sdk_java.ApiClient;
-import org.whispir.api.MessagesApi;
 import org.openapitools.client.model.Message;
 import org.whispir.api.MessagesApi;
 import whispir_sdk_java.ApiClient;
 import whispir_sdk_java.ApiException;
-import whispir_wrapped_java.Client;
 
 /**
  *
@@ -20,8 +17,7 @@ import whispir_wrapped_java.Client;
  */
 public class MessageClient {
 
-  static String USERNAME;
-  private static String PASSWORD, API_URL, WORKSPACE_ID;
+  private static String WORKSPACE_ID;
   private static final String CONTENT_TYPE = "application/vnd.whispir.message-v1+json";
   private static final String ACCEPT = "application/vnd.whispir.message-v1+json";
   private static ApiClient apiClientInit;
@@ -37,14 +33,12 @@ public class MessageClient {
     SimpleDateFormat dateFor = new SimpleDateFormat("dd/MM/YYYY HH:mm");
     calendar.add(Calendar.DAY_OF_WEEK, 1);
 
-    System.out.println(dateFor.format(calendar.getTime()));
-    
-//    if(MESSAGE.getMessageType().isEmpty()  )
-
-    MESSAGE.messageType(Message.MessageTypeEnum.SCHEDULED);
-    MESSAGE.scheduleType(Message.ScheduleTypeEnum.ONCE);
-    MESSAGE.scheduleDate(dateFor.format(calendar.getTime()));
-
+      if(MESSAGE.getMessageType() == null || MESSAGE.getScheduleType() == null || MESSAGE.getScheduleDate() == null) {
+        MESSAGE.messageType(Message.MessageTypeEnum.SCHEDULED);
+        MESSAGE.scheduleType(Message.ScheduleTypeEnum.ONCE);
+        MESSAGE.scheduleDate(dateFor.format(calendar.getTime()));
+      }
+      
     try {
       return message.postMessages(WORKSPACE_ID, API_KEY, CONTENT_TYPE, ACCEPT, MESSAGE);
     } catch (ApiException e) {
