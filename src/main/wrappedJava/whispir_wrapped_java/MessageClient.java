cacf56724cb45ffc5e17b32c8f90deef5c233ad9
coralewh <corale@whispir.com>
2022-10-24 16:24:51 +0800

    feat: remove unused classes

--- src/main/wrappedJava/whispir_wrapped_java/MessageClient.java
+++ src/main/wrappedJava/whispir_wrapped_java/MessageClient.java
@@ -23,10 +23,8 @@ import whispir_wrapped_java.Client;
 public class MessageClient {
 
   private static String WORKSPACE_ID;
-  private static final String CONTENT_TYPE =
-    "application/vnd.whispir.message-v1+json";
-  private static final String ACCEPT =
-    "application/vnd.whispir.message-v1+json";
+  private static final String CONTENT_TYPE = "application/vnd.whispir.message-v1+json";
+  private static final String ACCEPT = "application/vnd.whispir.message-v1+json";
   private static ApiClient apiClientInit;
 
   public MessageClient(ApiClient client, String workspaceId) {
@@ -40,45 +38,42 @@ public class MessageClient {
     SimpleDateFormat dateFor = new SimpleDateFormat("dd/MM/YYYY HH:mm");
     calendar.add(Calendar.DAY_OF_WEEK, 1);
 
-    if(MESSAGE.getMessageType() == null || MESSAGE.getScheduleType() == null || MESSAGE.getScheduleDate() == null) {
+    if (MESSAGE.getMessageType() == null || MESSAGE.getScheduleType() == null || MESSAGE.getScheduleDate() == null) {
 
-    //    if(MESSAGE.getMessageType().isEmpty()  )
-    MESSAGE.messageType(Message.MessageTypeEnum.SCHEDULED);
-    MESSAGE.scheduleType(Message.ScheduleTypeEnum.ONCE);
-    MESSAGE.scheduleDate(dateFor.format(calendar.getTime()));
+      MESSAGE.messageType(Message.MessageTypeEnum.SCHEDULED);
+      MESSAGE.scheduleType(Message.ScheduleTypeEnum.ONCE);
+      MESSAGE.scheduleDate(dateFor.format(calendar.getTime()));
+    }
 
     try {
       return message.postMessages(
-        WORKSPACE_ID,
-        API_KEY,
-        CONTENT_TYPE,
-        ACCEPT,
-        MESSAGE
-      );
+          WORKSPACE_ID,
+          API_KEY,
+          CONTENT_TYPE,
+          ACCEPT,
+          MESSAGE);
     } catch (ApiException e) {
       throw new Error(e);
     }
   }
 
   public static GetMessages200Response listMessages(
-    String API_KEY,
-    BigDecimal LIMIT,
-    BigDecimal OFFSET,
-    String SORT_ORDER,
-    String SORT_FIELDS
-  ) {
+      String API_KEY,
+      BigDecimal LIMIT,
+      BigDecimal OFFSET,
+      String SORT_ORDER,
+      String SORT_FIELDS) {
     MessagesApi message = new MessagesApi(apiClientInit);
 
     try {
       return message.getMessages(
-        WORKSPACE_ID,
-        API_KEY,
-        ACCEPT,
-        LIMIT,
-        OFFSET,
-        SORT_ORDER,
-        SORT_FIELDS
-      );
+          WORKSPACE_ID,
+          API_KEY,
+          ACCEPT,
+          LIMIT,
+          OFFSET,
+          SORT_ORDER,
+          SORT_FIELDS);
     } catch (ApiException e) {
       throw new Error(e);
     }
@@ -94,60 +89,56 @@ public class MessageClient {
   }
 
   public static MessageStatus retrieveMessageStatus(
-    String API_KEY,
-    String MESSAGE_ID,
-    BigDecimal LIMIT,
-    BigDecimal OFFSET,
-    String SORT_ORDER,
-    String SORT_FIELDS,
-    String VIEW,
-    String FILTER
-  ) {
+      String API_KEY,
+      String MESSAGE_ID,
+      BigDecimal LIMIT,
+      BigDecimal OFFSET,
+      String SORT_ORDER,
+      String SORT_FIELDS,
+      String VIEW,
+      String FILTER) {
     MessagesApi message = new MessagesApi(apiClientInit);
 
     try {
       return message.getMessageStatus(
-        WORKSPACE_ID,
-        API_KEY,
-        ACCEPT,
-        MESSAGE_ID,
-        LIMIT,
-        OFFSET,
-        SORT_ORDER,
-        SORT_FIELDS,
-        VIEW,
-        FILTER
-      );
+          WORKSPACE_ID,
+          API_KEY,
+          ACCEPT,
+          MESSAGE_ID,
+          LIMIT,
+          OFFSET,
+          SORT_ORDER,
+          SORT_FIELDS,
+          VIEW,
+          FILTER);
     } catch (ApiException e) {
       throw new Error(e);
     }
   }
 
   public static GetMessageResponsesById200Response retrieveMessageResponse(
-    String API_KEY,
-    String MESSAGE_ID,
-    BigDecimal LIMIT,
-    BigDecimal OFFSET,
-    String SORT_ORDER,
-    String SORT_FIELDS,
-    String VIEW,
-    String FILTER
-  ) {
+      String API_KEY,
+      String MESSAGE_ID,
+      BigDecimal LIMIT,
+      BigDecimal OFFSET,
+      String SORT_ORDER,
+      String SORT_FIELDS,
+      String VIEW,
+      String FILTER) {
     MessagesApi message = new MessagesApi(apiClientInit);
 
     try {
       return message.getMessageResponsesById(
-        WORKSPACE_ID,
-        API_KEY,
-        ACCEPT,
-        MESSAGE_ID,
-        LIMIT,
-        OFFSET,
-        SORT_ORDER,
-        SORT_FIELDS,
-        VIEW,
-        FILTER
-      );
+          WORKSPACE_ID,
+          API_KEY,
+          ACCEPT,
+          MESSAGE_ID,
+          LIMIT,
+          OFFSET,
+          SORT_ORDER,
+          SORT_FIELDS,
+          VIEW,
+          FILTER);
     } catch (ApiException e) {
       throw new Error(e);
     }
