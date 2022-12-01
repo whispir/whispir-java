2e93280a356c52de505213ae3e96ec26f125baf5
Harry Utomo <hutomo@whispir.com>
2022-12-01 12:59:23 +1100

    Updated sample java client implementation to adopt param builder instance

--- src/main/wrappedJava/whispir_wrapped_java/WrappedJava.java
+++ src/main/wrappedJava/whispir_wrapped_java/WrappedJava.java
@@ -1,25 +1,46 @@
 package whispir_wrapped_java;
 import org.openapitools.client.model.Message;
+import io.github.cdimascio.dotenv.Dotenv;
+import whispir_sdk_java.ApiClient;
+import whispir_sdk_java.ApiException;
+
+import org.whispir.api.MessagesApi;
+import whispir_sdk_java.param.MessageCreateParams;
 
 public class WrappedJava {
     public static void main(String[] args) {
-        WhispirClient whispir = new WhispirClient(
-            "https://api.au.whispir.com",
-            "username",
-            "password", 
-            "workspace_id", 
-            "apikey"
-        );
+        String workspaceId = "74C38E61A371730D"; // A Space Odessey
+        Dotenv dotenv = Dotenv.load();
 
-        // Sending SMS Message
-        String recipientPhoneNumber = "phone_number";
-        String subject = "First Message";
-        String body = "You have sent your first message from the Whispir SDK. Congrats!";
+        ApiClient apiClient = new ApiClient();
+        apiClient.setBasePath(dotenv.get("API_URL"));
+        apiClient.setApiKey(dotenv.get("API_KEY"));
+        apiClient.setUsername(dotenv.get("WHISPIR_USERNAME"));
+        apiClient.setPassword(dotenv.get("WHISPIR_PASSWORD"));
 
 
-        MessageClient messages = whispir.messageClient();
-        Message resp = messages.create(recipientPhoneNumber, subject, body);
-        System.out.println("Send Message Response: " + resp);
+        MessageCreateParams params = new MessageCreateParams.Builder()
+                                    .workspaceId(workspaceId)
+                                    .to("123")
+                                    .subject("subject")
+                                    .body("message bod").build();
 
+        Message payload = new Message();
+        payload.to("61478107505");
+        payload.subject("First Message");
+        payload.body("You have sent your first message from the Whispir SDK. Congrats!");
+
+
+        MessagesApi messages = new MessagesApi(apiClient);
+        try {
+            Message response = messages.create(params);
+            // System.out.println(response.getHeaders().get("Location"));
+        } catch (ApiException e) {
+            System.err.println("Exception when calling Send SMS Message API");
+            System.err.println("Status code: " + e.getCode());
+            System.err.println("Reason: " + e.getResponseBody());
+            System.err.println("Response headers: " + e.getResponseHeaders());
+            e.printStackTrace();
+        }
     }
 }
