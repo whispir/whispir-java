2ee287f5927854e7f002fbd3d9df85777f6483b7
mneri <mneri@whispir.com>
2023-02-16 06:40:38 +0800

    moved directory of test

--- src/test/java/com/whispir/client/ApiClientTest.java
+++ src/test/java/com/whispir/client/ApiClientTest.java
@@ -1,7 +1,9 @@
 package com.whispir.client;
 
 import org.junit.jupiter.api.Disabled;
+
 import com.whispir.client.ApiException;
+// import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import com.whispir.Whispir;
 import com.whispir.api.WorkspacesApi;
@@ -12,11 +14,19 @@ import com.whispir.api.ContactsApi.ContactCreateParams;
 import com.whispir.api.ContactsApi.ContactCreateParams.MessagingOption.ChannelEnum;
 import com.whispir.api.MessagesApi.MessageCreateParams;
 import com.whispir.api.WorkspacesApi.WorkspaceListParams;
+// import com.whispir_java
 import com.whispir.client.ApiClient;
 import com.whispir.client.ApiException;
+// import whispir_java.Client;
 import com.whispir.client.Configuration;
 import com.whispir.client.auth.*;
-
+// import whispir_java.models.*;
+// import com.whispir.api.*;
+// import com.whispir.api.MessagesApi;
+// import com.whispir.api.WorkspacesApi;
+// import com.whispir.api.ContactsApi;
+// import com.whispir.api.*;
+// import com.whispir.client.models.*;
 import com.whispir.model.*;
 // @Disabled
 public class ApiClientTest {
@@ -32,12 +42,18 @@ public class ApiClientTest {
             WorkspaceListParams params = new WorkspaceListParams.Builder().build();
             WorkspaceCollection workspaces = WorkspacesApi.list(params);
             WorkspaceRetrieveParams getparams = new WorkspaceRetrieveParams.Builder()
-            .setWorkspaceId(workspaceId)
+            .setWorkspaceId("9EE1EBE2FBA5660B")
             .build();
             Workspace WORKSPACE = WorkspacesApi.retrieve(getparams);
             System.out.println(">>>>" + WORKSPACE);
+            // Message response = MessagesApi.create(params2);
+            // Contact response = ContactsApi.create(params);
+            // System.out.println(response.lastResponse.getHeaders());
         } catch (ApiException e) {
             System.out.println("Exception when calling MessagesApi#getMessageById");
+            // System.out.println("Status code: " + e.getCode());
+            // System.out.println("Reason: " + e.getResponseBody());
+            // System.out.println("Response headers: " + e.getResponseHeaders());
             e.printStackTrace();
         }
     }
