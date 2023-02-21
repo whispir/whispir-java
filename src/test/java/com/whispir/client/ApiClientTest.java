318e9f90160c7129a3db697ddfeb0f889293f89e
mneri <107977941+mneri-whispir@users.noreply.github.com>
2023-02-22 05:48:35 +0800

    feat: ci updates (#20)
    Co-authored-by: Richard Fontein <32132657+richardfontein@users.noreply.github.com>

---
+++ src/test/java/com/whispir/client/ApiClientTest.java
@@ -0,0 +1,43 @@
+package com.whispir.client;
+import org.junit.jupiter.api.Disabled;
+import com.whispir.client.ApiException;
+import org.junit.jupiter.api.Test;
+import com.whispir.Whispir;
+import com.whispir.api.WorkspacesApi;
+import com.whispir.api.WorkspacesApi.WorkspaceRetrieveParams;
+import com.whispir.api.ContactsApi;
+import com.whispir.api.MessagesApi;
+import com.whispir.api.ContactsApi.ContactCreateParams;
+import com.whispir.api.ContactsApi.ContactCreateParams.MessagingOption.ChannelEnum;
+import com.whispir.api.MessagesApi.MessageCreateParams;
+import com.whispir.api.WorkspacesApi.WorkspaceListParams;
+import com.whispir.client.ApiClient;
+import com.whispir.client.ApiException;
+import com.whispir.client.Configuration;
+import com.whispir.client.auth.*;
+
+import com.whispir.model.*;
+
+public class ApiClientTest {
+    @Test
+    public void RunTest() throws ApiException {
+        String workspaceId = "26E4E27F0360A8C9";
+        Whispir.apiKey = "apiKeyMock";
+        Whispir.username = "whispir_user";
+        Whispir.password = "whispir_password";
+        Whispir.host = "http://127.0.0.1:4010";
+        try {
+            WorkspaceListParams params = new WorkspaceListParams.Builder().build();
+            WorkspaceCollection workspaces = WorkspacesApi.list(params);
+            WorkspaceRetrieveParams getparams = new WorkspaceRetrieveParams.Builder()
+            .setWorkspaceId("9EE1EBE2FBA5660B")
+            .build();
+            Workspace WORKSPACE = WorkspacesApi.retrieve(getparams);
+            System.out.println("Workspace found:" + WORKSPACE);
+
+        } catch (ApiException e) {
+            System.out.println("Exception when calling WorkspacesApi#retrieve");
+            e.printStackTrace();
+        }
+    }
+}
